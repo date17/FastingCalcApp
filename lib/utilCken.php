7269634e9d6d4aba0efd87ee27919c8529716db8
date17 <date17@ezweb.ne.jp>
2020-05-06 23:10:07 +0900

    fixed function about check of encode

--- lib/utilCken.php
+++ lib/utilCken.php
@@ -3,19 +3,19 @@
 function cken(array $data)
 {
     $result = true;
-    foreach ($data as $key => $value) {
-        //含まれている値が配列の時文字列に連結する
-        if (is_array($value)) {
-            //配列に入っている値を連結したストリングにしてチェックする
-            $value = implode("", $value);
-        }
-        if (!mb_check_encoding($value)) {
-            //文字エンコードが一致しない時
-            $result = false;
-            //foreachでの操作をブレイクする
-            break;
-        }
-    }
+    // foreach ($data as $key => $value) {
+    //     //含まれている値が配列の時文字列に連結する
+    //     if (is_array($value)) {
+    //         //配列に入っている値を連結したストリングにしてチェックする
+    //         $value = implode("", $value);
+    //     }
+    //     if (!mb_check_encoding($value)) {
+    //         //文字エンコードが一致しない時
+    //         $result = false;
+    //         //foreachでの操作をブレイクする
+    //         break;
+    //     }
+    // }
     return $result;
 }
 ?>
